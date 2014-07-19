60a533c39e84e67737e7e91ea929b78a7eeb4300
Neal Sidhwaney <nealsid+github@gmail.com>
2014-07-19 16:06:47 -0400

    Initial Commit

---
+++ Nomad Clinic Health Records/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Nomad Clinic Health Records
+//
+//  Created by Neal Sidhwaney on 7/19/14.
+//  Copyright (c) 2014 Upaya Zen Center. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
---
+++ Nomad Clinic Health RecordsTests/Nomad_Clinic_Health_RecordsTests.m
@@ -0,0 +1,39 @@
+//
+//  Nomad_Clinic_Health_RecordsTests.m
+//  Nomad Clinic Health RecordsTests
+//
+//  Created by Neal Sidhwaney on 7/19/14.
+//  Copyright (c) 2014 Upaya Zen Center. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Nomad_Clinic_Health_RecordsTests : XCTestCase
+
+@end
+
+@implementation Nomad_Clinic_Health_RecordsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

c3f1961241d204446626b911dd5b98076ef8b9ac
Neal <nealsid@gmail.com>
2014-09-03 22:16:50 -0400

    Initial version of BaseStore to refactor all common code inside store objects

---
+++ Nomad Clinic Health Records/BaseStoreTest.m
@@ -0,0 +1,31 @@
+//
+//  BaseStoreTest.m
+//  Nomad Clinic Health Records
+//
+//  Created by Neal Sidhwaney on 9/3/14.
+//  Copyright (c) 2014 Upaya Zen Center. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BaseStoreTest : XCTestCase
+
+@end
+
+@implementation BaseStoreTest
+
+- (void)setUp {
+  [super setUp];
+  // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+  // Put teardown code here. This method is called after the invocation of each test method in the class.
+  [super tearDown];
+}
+
+- (void)testExample {
+  XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
